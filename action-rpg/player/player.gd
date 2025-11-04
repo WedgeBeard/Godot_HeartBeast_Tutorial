68e9d2dff5f401eea01694ea5dfd6b07995d41aa
WedgeBeard <tysonstecklein@gmail.com>
2025-11-04 16:43:43 -0700

    updated character movenent

--- action-rpg/player/player.gd
+++ action-rpg/player/player.gd
@@ -3,5 +3,9 @@ extends CharacterBody2D
 const SPEED = 100.0
 
 func _physics_process(delta: float) -> void:
-	velocity = Vector2.RIGHT * SPEED
+	velocity = Vector2.ZERO
+	
+	var input_vector = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	velocity = input_vector * SPEED
+		
 	move_and_slide()
